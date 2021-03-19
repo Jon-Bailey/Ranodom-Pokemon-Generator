ba53a2c1cd10c1cf971e32cc80f92363cb9bfe13
Jon Bailey <jonbailey788@gmail.com>
2021-03-18 23:07:18 -0500

    fixed for netlify

--- src/components/FetchRandomPokemon.js
+++ src/components/FetchRandomPokemon.js
@@ -4,19 +4,19 @@ import React, { useEffect, useState, } from 'react';
 const FetchPokemon = () => {
 
     const getNewPokemon = () => { 
-        let newPokemon = Math.floor(Math.random() * 150 + 1)
+        let newPokemon = Math.floor(Math.random() * 151 + 1)
         return(newPokemon);
     }
     const pokemonID = getNewPokemon();
     const [pokemon, setPokemon] = useState([]);
-    const [loading, setLoading] = useState(false);
+    //const [loading, setLoading] = useState(false);
 
     const getPokemon = async () => {
         //setLoading(true);
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}/`);
         const data = await response.json();
         setPokemon(data.name);
-        setLoading(false);
+        //setLoading(false);
         console.log(data.name);
     }
 
@@ -32,12 +32,13 @@ return(
         </div>
         <div className="main">
             <h1>{pokemon}</h1>
-            <button type="button" onClick={getNewPokemon()}><h2>This button doesn't work yet</h2></button>
-            <h3 className="instructions">you have to re-load the page to get a new pokemon</h3>
+            <h3 className="instructions">re-load the page for a new pokemon</h3>
+            <button type="button" onClick={getNewPokemon()}><h4>This button doesn't work yet</h4></button>
+            
         </div>
         <div className="header-footer">
-            <h3>It kinda works</h3>
-            <h3>2021</h3>
+            <h3>151 pokemon</h3>
+            <h3>work in progress...</h3>
         </div>
     </div>
     )}
