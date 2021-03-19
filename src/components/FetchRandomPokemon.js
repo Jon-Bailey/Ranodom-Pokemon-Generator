9149be6c4803c072ca1b226796c8fb1a009c38de
Jon Bailey <jonbailey788@gmail.com>
2021-03-18 22:55:46 -0500

    styling

--- src/components/FetchRandomPokemon.js
+++ src/components/FetchRandomPokemon.js
@@ -25,9 +25,20 @@ const FetchPokemon = () => {
       })
 
 return(
-    <div>
-        <h1>{pokemon}</h1>
-        <button type="button" onClick={getNewPokemon()}><h2>New Pokemon</h2></button>
+    <div className="container">
+        <div className="header-footer">
+            <h3>Pokemon Name Generator</h3>
+            <h3>Gotta Draw 'em all</h3>
+        </div>
+        <div className="main">
+            <h1>{pokemon}</h1>
+            <button type="button" onClick={getNewPokemon()}><h2>This button doesn't work yet</h2></button>
+            <h3 className="instructions">you have to re-load the page to get a new pokemon</h3>
+        </div>
+        <div className="header-footer">
+            <h3>It kinda works</h3>
+            <h3>2021</h3>
+        </div>
     </div>
     )}
     
