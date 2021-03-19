c7f4a41ffa0a59eb4f38f48d9761b1b32986d32d
Jon Bailey <jonbailey788@gmail.com>
2021-03-19 14:07:18 -0500

    Loading and generation constants

--- src/components/FetchRandomPokemon.js
+++ src/components/FetchRandomPokemon.js
@@ -4,11 +4,20 @@ import React, { useState, } from 'react';
 const FetchPokemon = () => {
 
     const [pokemon, setPokemon] = useState();
+    const [loading, setLoading] = useState();
+
+    //determines how many generations to fetch
+    //const gen1 = 150 + 1;
+    //const gen2 = 251 + 1;
+    //const gen3 = 386 + 1;
+    const gen4 = 493 + 1;
 
     const getPokemon = async () => {
-        let newPokemon = Math.floor(Math.random() * 151 + 1)
+        setLoading(true);
+        let newPokemon = Math.floor(Math.random() * gen4)
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${newPokemon}/`);
         const data = await response.json();
+        setLoading(false);
         setPokemon(data.name);
         console.log(data.name);
     }
@@ -20,11 +29,17 @@ return(
             <h3>Gotta Draw 'em all</h3>
         </div>
         <div className="main">
-            <h1>{pokemon}</h1>
-            <button type="button" onClick={getPokemon}><h4>New Pokemon</h4></button>    
+            <div className="name">
+                {loading ? <h3>Loading...</h3> : <h1>{pokemon}</h1>}
+            
+            <button type="button" onClick={getPokemon}><h4>New Pokemon</h4></button> 
+            </div>
+            <div className="image">
+                
+            </div>
         </div>
         <div className="header-footer">
-            <h3>151 pokemon</h3>
+            <h3>Generations 1 - 4</h3>
             <h3>work in progress...</h3>
         </div>
     </div>
